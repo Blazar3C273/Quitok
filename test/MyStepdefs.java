810453f157529008885f61b1d18689ce2539f49b
Stepanenko Anatoliy <tolik.stepanenko@gmail.com>
2015-05-20 21:45:23 +0300

    test

---
+++ test/MyStepdefs.java
@@ -0,0 +1,17 @@
+import cucumber.api.java.ru.Дано;
+import org.apache.log4j.Logger;
+
+
+/**
+ * Quitok.
+ * Created by Anatoliy on 20.05.2015.
+ */
+public class MyStepdefs {
+    private static final Logger log = Logger.getLogger(MyStepdefs.class);
+
+    @Дано("^Окно авторизации$")
+    public void Окно_авторизации() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new cucumber.api.PendingException();
+    }
+}
